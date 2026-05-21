f4f539ccc83283b44c61a382d9ed07ae7d5e2aa2
YangSiYoung <50095992+Yangsiyoung0901@users.noreply.github.com>
2026-05-21 14:18:45 +0900

    평가 데이터 셋 및 테스트, 평가, 결과 출력 코드

--- YSY/ODTEST.py
+++
@@ -1,5 +0,0 @@
-from ultralytics import YOLO
-m = YOLO(r"D:\AIProject\YSY\best.pt")
-r = m.predict(r"D:\AIProject\YSY\gt_test_seperate\gt_test\originals\large\images\a02087.jpg", conf=0.05, verbose=False)[0]  # conf 아주 낮게
-for b in r.boxes:
-    print(m.names[int(b.cls[0])], round(float(b.conf[0]), 3))
